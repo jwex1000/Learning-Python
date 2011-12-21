92897a29b625ac52431dfe6c13044e026f0e568c
Joshua Wexler <josh@occomgroup.com>
2011-12-21 16:07:11 -0500

    Start of python planet

---
+++ learn_python_the_hard_way_ex/ex37.py
@@ -0,0 +1,11 @@
+def start():
+    print "Welcome to Python Plant"
+    print "This is a scary game that is all text! WHOO!"
+    print "Get ready to have some fun!"
+    
+    print "What is your name?"
+    user_name = raw_input ("> ")
+    
+    print "Welcome %s" % (user_name)
+
+start()
